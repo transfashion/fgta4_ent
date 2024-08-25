8acaf7abee119b6b1d5ffe3f7458cc4f45a7389c
agungdhewe <agung_dhewe@yahoo.com>
2024-08-25 20:43:09 +0700

    move items to ent

---
+++ items/item/item.apis.mjs
@@ -0,0 +1,17 @@
+/* API yang akan digunakan di program
+
+
+	export const testapi = 'fgta/framework/formdev/nama-api'
+
+
+kemdian, di program bisa diakses dengan
+
+	$ui.apis.testapi
+
+*/
+
+export const load_itemgroup_id = 'ent/items/itemgroup/list'
+export const load_itemclass_id = 'ent/items/itemclass/list'
+export const load_dept_id = 'ent/organisation/dept/list'
+
+
---
+++ items/itemstock/itemstock.apis.mjs
@@ -0,0 +1,22 @@
+/* API yang akan digunakan di program
+
+
+	export const testapi = 'fgta/framework/formdev/nama-api'
+
+
+kemdian, di program bisa diakses dengan
+
+	$ui.apis.testapi
+
+*/
+
+export const load_itemgroup_id = 'ent/items/itemgroup/list'
+export const load_itemclass_id = 'ent/items/itemclass/list'
+export const load_dept_id = 'ent/organisation/dept/list'
+export const load_unit_id = 'ent/organisation/unit/list'
+export const load_unitmeasurement_id = 'ent/general/unitmeasurement/list'
+export const load_itemproptype_id = 'ent/items/itemproptype/list'
+export const load_itemstock_compound = 'local: list'
+export const load_itemstock_conversion = 'local: list'
+
+
