909460a76558db37fea983f8b44d550859b7b983
agungdhewe <agung_dhewe@yahoo.com>
2024-08-27 13:45:57 +0700

    fix table empluser

--- organisation/empluser/empluser.sql
+++ organisation/empluser/empluser.sql
@@ -3,17 +3,18 @@
 CREATE TABLE `mst_empluser` (
   `empl_id` varchar(30) NOT NULL,
   `user_id` varchar(14) NOT NULL,
-	`_createby` varchar(13) NOT NULL , 
-	`_createdate` datetime NOT NULL DEFAULT current_timestamp(), 
-	`_modifyby` varchar(13)  , 
-	`_modifydate` datetime  ,   
+  `empluser_allowviewalldept` tinyint(1) NOT NULL DEFAULT 0,
+  `empluser_allowviewallunit` tinyint(1) NOT NULL DEFAULT 0,
+  `empluser_allowviewallsite` tinyint(1) NOT NULL DEFAULT 0,
+  `_createby` varchar(13) NOT NULL,
+  `_createdate` datetime NOT NULL DEFAULT current_timestamp(),
+  `_modifyby` varchar(13) DEFAULT NULL,
+  `_modifydate` datetime DEFAULT NULL,
   PRIMARY KEY (`empl_id`),
   UNIQUE KEY `mst_empluser_user_id_IDX` (`user_id`) USING BTREE,
   CONSTRAINT `mst_empluser_fgt_user` FOREIGN KEY (`user_id`) REFERENCES `fgt_user` (`user_id`),
   CONSTRAINT `mst_empluser_mst_empl` FOREIGN KEY (`empl_id`) REFERENCES `mst_empl` (`empl_id`)
-) 
-ENGINE=InnoDB
-COMMENT='Assign Employee dengan user_id';
+) ENGINE=InnoDB COMMENT='Assign Employee dengan user_id';
 
 
 
